36014d1cc9b5e279827a34f00bdbd822884c3e4b
Maybay <125787441+thanatthanan@users.noreply.github.com>
2026-06-22 20:46:53 +0700

    fix: Explicitly set collation to utf8mb4_unicode_ci to support emojis on prod host

--- config/database.js
+++ config/database.js
@@ -10,6 +10,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'family_expense',
   charset: 'utf8mb4',
+  collation: 'utf8mb4_unicode_ci',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
